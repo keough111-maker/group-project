ad1077a0ba5eb0a0f03e143dea76b4c3d79e5042
keough111-maker <keough111@gmail.com>
2025-12-15 22:00:45 +0800

    Update group project.py

--- group project.py
+++ group project.py
@@ -1,1 +1,6 @@
-
+# Cell 1: 安装依赖库
+print("⏳ 正在安装依赖库，请稍等...")
+# 安装 Hugging Face 相关库及音频处理库
+!pip install datasets transformers accelerate librosa evaluate torch soundfile==0.12.1 --quiet
+!pip install --upgrade accelerate --quiet
+print("✅ 环境安装完成！")
